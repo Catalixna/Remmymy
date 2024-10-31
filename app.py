f58457ec86e5b3dca7b7a9d23ae7856b7c8bd3f8
Catalixna <ccardenasaa@gmail.com>
2024-10-31 11:22:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ image = Image.open('Remmy.png')
 # Usar columnas para centrar la imagen
 col1, col2, col3 = st.columns([1, 2, 1])  # Ajusta las proporciones según necesites
 with col2:
-    st.image(image, caption='Tu imagen aquí', width=300)  # Usa 'image' en lugar de 'img'
+    st.image(image, caption='Tu receta a un clic', width=300)  # Usa 'image' en lugar de 'img'
 
 # Texto interactivo
 st.write("¿Qué quieres preparar el día de hoy?")
