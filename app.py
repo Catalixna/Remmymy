eeba8aa160716ed43f2be279e87789d87ddc0ac3
Catalixna <ccardenasaa@gmail.com>
2024-10-31 11:21:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ from PIL import Image
 
 # Cargar la imagen
 image = Image.open('Remmy.png')
-st.image(image, caption='.', use_column_width=True)
 
 # Usar columnas para centrar la imagen
 col1, col2, col3 = st.columns([1, 2, 1])  # Ajusta las proporciones según necesites
 with col2:
-    st.image(img, width=300)
+    st.image(image, caption='Tu imagen aquí', width=300)  # Usa 'image' en lugar de 'img'
 
 # Texto interactivo
 st.write("¿Qué quieres preparar el día de hoy?")
@@ -18,4 +17,3 @@ search_query = st.text_input("Buscar recetas:", "")
 
 if search_query:
     st.write(f"Buscando recetas para: **{search_query}**")
-
