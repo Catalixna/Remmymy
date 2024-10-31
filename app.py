193d8c4192e47ad05c5561cd4908b5fcc009778f
Catalixna <ccardenasaa@gmail.com>
2024-10-31 10:33:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 import streamlit as st
 from PIL import Image
 
-# Título centrado con estilo
-#st.markdown("<h1 style='text-align: center; color: #661200;'>TÚ chinito de confianza</h1>", unsafe_allow_html=True)
-
-# Cargar y mostrar la imagen
 image = Image.open('Remmy.png')
-st.image(image, caption='Tu imagen aquí', use_column_width=True)
+st.image(image, caption='.', use_column_width=True)
 
-# Texto interactivo
 st.write("¿Qué quieres preparar el día de hoy?")
 
 # Buscador
@@ -18,5 +13,4 @@ search_query = st.text_input("Buscar recetas:", "")
 if search_query:
     st.write(f"Buscando recetas para: **{search_query}**")
     # Aquí puedes agregar la lógica para filtrar o mostrar recetas basadas en la búsqueda
-else:
-    st.write("Introduce un ingrediente o tipo de comida para buscar recetas.")
+
