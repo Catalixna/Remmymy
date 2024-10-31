50bf4d0190c07ff2220f4980876312645c7bf5ad
Catalixna <ccardenasaa@gmail.com>
2024-10-31 10:31:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 from PIL import Image
 
-#st.markdown("<h1 style='text-align: center; color: #661200;'>TÚ chinito de confianza</h1>", unsafe_allow_html=True)
+# Título centrado con estilo
+st.markdown("<h1 style='text-align: center; color: #661200;'>TÚ chinito de confianza</h1>", unsafe_allow_html=True)
 
+# Cargar y mostrar la imagen
 image = Image.open('Remmy.png')
+st.image(image, caption='Tu imagen aquí', use_column_width=True)
 
- st.write("¿Que quieres preparar el día de hoy?")
-
+# Texto interactivo
+st.write("¿Qué quieres preparar el día de hoy?")
